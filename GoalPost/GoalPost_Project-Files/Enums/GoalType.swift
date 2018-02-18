7055f8310fc5c75de00c00dad7674aa058f2be24
Hozaifa Owaisi <hozaifaowaisi.o@gmail.com>
2018-02-18 15:45:14 -0500

    Add files via upload

---
+++ GoalPost/GoalPost_Project-Files/Enums/GoalType.swift
@@ -0,0 +1,13 @@
+//
+//  GoalType.swift
+//  GoalPost
+//
+//  Created by Hozaifa on 2/11/18.
+//  Copyright © 2018 Hozaifa. All rights reserved.
+//
+
+import Foundation
+enum GoalType: String {
+    case ShortTerm = "Short Term"
+    case LongTerm = "Long Term"
+}
